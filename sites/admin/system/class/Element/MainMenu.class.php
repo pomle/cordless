020570fd6c15fb9ffaba2ce8481e4ef19ca2a428
Pom <pontus.alexander@gmail.com>
2012-01-05 01:49:45 +0100

    - Added more comments to \Element\MainMenu::filterBranch() - GenerateMenu tool now respects PHP __line__ comments, i.e. # and //

--- sites/admin/system/class/Element/MainMenu.class.php
+++ sites/admin/system/class/Element/MainMenu.class.php
@@ -44,23 +44,36 @@ class MainMenu extends Common\Root
 
 	public function filterBranch(Array &$branch, $policies)
 	{
+		### Our default position is that we are going to remove this whole $branch from the tree
 		$hasItemsLeft = false;
 
 		foreach($branch as $key => &$item)
 		{
+			### A numeric key means this is an actual item
 			if( is_numeric($key) )
 			{
+				### Item shouldn't be accessible based on the current policy set, so remove it
 				if( isset($item['policy']) && !in_array($item['policy'], $policies) )
+				{
+					#printf("Removing \"%s\"\n", $item['caption']);
 					unset($branch[$key]);
+				}
+				### Report to back that there are items in here that should be displayed, so branch has to be kept
 				else
+				{
 					$hasItemsLeft = true;
+				}
 			}
 			else
 			{
-				if( $this->filterBranch($item, $policies) )
-					$hasItemsLeft = true;
+				### We are on a branch, and delving deeper, and if there are items in here, this current branch should also be kept
+				#printf("Delving into \"%s\"\n", $key);
+				if( ($hasItemsLeft = $this->filterBranch($item, $policies)) === false )
+					unset($branch[$key]);
 			}
 		}
+
+		#printf("Returning: %u\n", $hasItemsLeft);
 
 		return $hasItemsLeft;
 	}
@@ -68,6 +81,7 @@ class MainMenu extends Common\Root
 	public function filterPolicies($policies)
 	{
 		$this->filterBranch($this->tree, $policies);
+		return true;
 	}
 
 	public function getAsVariable()
@@ -80,22 +94,25 @@ class MainMenu extends Common\Root
 
 	public function printBranch(Array $branch, $title = null)
 	{
-		if( $title ) printf('%s', htmlspecialchars($title));
-
-		echo "<ul>";
-
-		foreach($branch as $key => $item)
+		if( count($branch) > 0 )
 		{
-			echo '<li>';
+			if( $title ) printf('%s', htmlspecialchars($title));
 
-			if( is_numeric($key) )
-				printf('<a href="%s">%s</a>', $item['href'], $item['caption']);
-			else
-				$this->printBranch($item, $key);
+			echo "<ul>";
 
-			echo '</li>';
+			foreach($branch as $key => $item)
+			{
+				echo '<li>';
+
+				if( is_numeric($key) )
+					printf('<a href="%s">%s</a>', $item['href'], $item['caption']);
+				else
+					$this->printBranch($item, $key);
+
+				echo '</li>';
+			}
+
+			echo "</ul>";
 		}
-
-		echo "</ul>";
 	}
 }
