b046405765134d830c23fb9dfbf8b20c5e7df6a0
Pom <pontus.alexander@gmail.com>
2012-01-08 04:09:17 +0100

    - Moved Operation namespace from Admin to Framework - Added __isset() method to UserSetting class

--- framework/class/UserSetting.class.php
+++ framework/class/UserSetting.class.php
@@ -14,12 +14,15 @@ class UserSetting
 		return isset($this->settings[$key]) ? $this->settings[$key] : null;
 	}
 
-	### Notice that value is only set if not null
-	public function __set($key, $value)
+	public function __isset($key)
+	{
+		return isset($this->settings[$key]);
+	}
+
+	public function __set($key, $value) ### Notice value is only set if not null
 	{
 		if( !is_null($value) )
 			$this->settings[$key] = $value;
-
 		else
 			unset($this->settings[$key]);
 	}
