664a3615b8eae6440cf845a1e1256a1e9c46e354
Pom <pontus.alexander@gmail.com>
2012-03-20 21:00:39 +0100

    - Cleaned up unnecessary garbage som site Init

--- sites/Init.inc.php
+++ sites/Init.inc.php
@@ -1,10 +1,3 @@
 <?
-namespace Asenine;
-
 require __DIR__ . '/../Init.inc.php';
-
 require ASENINE_DIR_INCLUDE . 'Functions.Site.inc.php';
-
-define('DIR_SYSTEM', ASENINE_DIR_ROOT . 'system/');
-
-addIncludePath(DIR_SYSTEM . 'class/');
