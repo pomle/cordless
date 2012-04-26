f944311565943317904af8b4ed907eec5134d21f
Pom <pontus.alexander@gmail.com>
2012-04-26 08:59:24 +0200

    - Login page now displays track image and caption if $_GET['userTrackID'] is set

--- sites/player/system/element/Login.inc.php
+++ sites/player/system/element/Login.inc.php
@@ -14,33 +14,57 @@ if( isset($_POST['login']) )
 
 $username = isset($_COOKIE['username']) ? $_COOKIE['username'] : '';
 
+
 include DIR_ELEMENT . 'Header.Outside.inc.php';
+
+if( isset($_GET['userTrackID']) && $UserTrack = UserTrack::loadFromDB($_GET['userTrackID']) )
+{
+	$imageURL = null;
+	if( isset($UserTrack->Image) )
+		$imageURL = \Asenine\Media\Producer\Thumb::createFromHash($UserTrack->Image->mediaHash)->getCustom(200, 200, true);
+
+	?>
+	<section class="track">
+
+		<img src="<? echo $imageURL; ?>">
+
+		<h1><? echo htmlspecialchars($UserTrack->title); ?></h1>
+		<h2><? echo htmlspecialchars($UserTrack->artist); ?></h2>
+
+	</section>
+	<?
+}
 ?>
-<h1><? echo _("Login"); ?></h1>
 
-<a href="./SignUp.php"><? echo _("I have an invite"), ' &raquo;'; ?></a>
+<section class="login">
 
-<form action="<? echo getenv('REQUEST_URI'); ?>" method="post">
-	<table class="login">
-		<tr>
-			<td>
-				<label><? echo _('Username'); ?></label><br>
-				<? echo \Asenine\Element\Input::text('username', $username); ?>
-			</td>
-		</tr>
-		<tr>
-			<td>
-				<label><? echo _('Password'); ?></label><br>
-				<? echo \Asenine\Element\Input::password('password')->addAttr('autocomplete', 'off'); ?>
-			</td>
-		</tr>
-		<tr>
-			<td colspan="2">
-				<button type="submit" name="login" value="1"><? echo _('Login'); ?></button>
-			</td>
-		</tr>
-	</table>
-</form>
+	<h1><? echo _("Login"); ?></h1>
+
+	<a href="./SignUp.php"><? echo _("I have an invite"), ' &raquo;'; ?></a>
+
+	<form action="<? echo getenv('REQUEST_URI'); ?>" method="post">
+		<table class="login">
+			<tr>
+				<td>
+					<label><? echo _('Username'); ?></label><br>
+					<? echo \Asenine\Element\Input::text('username', $username); ?>
+				</td>
+			</tr>
+			<tr>
+				<td>
+					<label><? echo _('Password'); ?></label><br>
+					<? echo \Asenine\Element\Input::password('password')->addAttr('autocomplete', 'off'); ?>
+				</td>
+			</tr>
+			<tr>
+				<td colspan="2">
+					<button type="submit" name="login" value="1"><? echo _('Login'); ?></button>
+				</td>
+			</tr>
+		</table>
+	</form>
+
+</section>
 <?
 include DIR_ELEMENT . 'Footer.Outside.inc.php';
 
