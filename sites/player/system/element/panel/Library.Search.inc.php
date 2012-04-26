5b34691cd2b42f7d370d2e010c9fa3e71f2d6d03
Pom <pontus.alexander@gmail.com>
2012-04-26 15:41:38 +0200

    - Fixed friend search bug

--- sites/player/system/element/panel/Library.Search.inc.php
+++ sites/player/system/element/panel/Library.Search.inc.php
@@ -1,56 +1,68 @@
 <?
 namespace Cordless;
 
-$queryMinLen = 2;
-$searchPreviewMaxLen = 8;
-$wildcards = array(' ', '*', '%');
+class SearchException extends \Exception{}
 
-$search = $params->q;
-
-if( preg_match('/^cordless:(.+):(.+)$/U', $search, $match) )
+try
 {
-	list(, $action, $arg) = $match;
+	$queryMinLen = 2;
+	$searchPreviewMaxLen = 8;
+	$wildcards = array(' ', '*', '%');
 
-	switch( strtolower($action) )
+	$search = $params->q;
+
+	if( preg_match('/^cordless:(.+):(.+)$/U', $search, $match) )
 	{
-		case 'user':
-			if( !$params->userID = \Asenine\User\Dataset::getUserID($arg) )
-				throw new PanelException(str_replace('%USERNAME%', $arg, _('User "%USERNAME%" not found')));
+		list(, $action, $arg) = $match;
 
-			libraryPanel('User-Overview', $params);
-			exit;
-		break;
+		switch( strtolower($action) )
+		{
+			case 'user':
+				if( !$params->userID = \Asenine\User\Dataset::getUserID($arg) )
+					throw new PanelException(str_replace('%USERNAME%', $arg, _('User "%USERNAME%" not found')));
 
-		case 'usertrack':
-			$params->userTrackID = (int)$arg;
-			libraryPanel('UserTrack-Control', $params);
-			exit;
-		break;
+				libraryPanel('User-Overview', $params);
+				exit;
+			break;
+
+			case 'usertrack':
+				$params->userTrackID = (int)$arg;
+				libraryPanel('UserTrack-Control', $params);
+				exit;
+			break;
+		}
 	}
+
+
+
+	$title = _("Search");
+	if( mb_strlen($search) > 0 ) $title .= sprintf(" (%s)", mb_strlen($search) > $searchPreviewMaxLen + 3 ? mb_substr($search, 0, $searchPreviewMaxLen) . '...' : $search);
+
+	echo Element\Library::head(
+		_('Search'),
+		$search,
+		$title
+	);
+
+	if( strlen(str_replace($wildcards, '', $search)) < $queryMinLen )
+		throw new SearchException(sprintf(_("Query too short. Minimum allowed length is %d _real_ characters"), $queryMinLen));
+
+	$query_search = str_replace($wildcards, '%', $search);
+
+	$userIDs = array_merge(array(USER_ID), $User->getFriendUserIDs());
+
+
+	$Fetcher = new Fetch\UserTrack($User, 'bySearch', $query_search, $userIDs);
+	$Fetcher->limit = 20;
+
+	$Tracklist = Element\Tracklist::createFromFetcher( $Fetcher );
+
+	if( $Tracklist->length == 0 )
+		throw new SearchException(sprintf(_("No matches found for \"%s\""), htmlspecialchars($search)));
+
+	echo $Tracklist;
 }
-
-
-
-$title = _("Search");
-if( mb_strlen($search) > 0 ) $title .= sprintf(" (%s)", mb_strlen($search) > $searchPreviewMaxLen + 3 ? mb_substr($search, 0, $searchPreviewMaxLen) . '...' : $search);
-
-echo Element\Library::head(
-	_('Search'),
-	$search,
-	$title
-);
-
-if( strlen(str_replace($wildcards, '', $search)) < $queryMinLen )
-	throw New \Exception(sprintf(_("Query too short. Minimum allowed length is %d _real_ characters"), $queryMinLen));
-
-$query_search = str_replace($wildcards, '%', $search);
-
-$Fetcher = new Fetch\UserTrack($User, 'bySearch', $query_search);
-$Fetcher->limit = 20;
-
-$Tracklist = Element\Tracklist::createFromFetcher( $Fetcher );
-
-if( $Tracklist->length == 0 )
-	throw New \Exception(sprintf(_("No matches found for \"%s\""), htmlspecialchars($search)));
-
-echo $Tracklist;
+catch(SearchException $e)
+{
+	printf('<p>%s</p>', $e->getMessage());
+}
